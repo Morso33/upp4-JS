0e51e7432febea95d1ec8b16d8fd97d00b8edd1f
Morso <69962221+Morso33@users.noreply.github.com>
2021-03-02 11:37:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var i;
+var Frukter = ["Äppel", "Banan", "Päron", "Drakfrukt"];
+console.log(Frukter)
+console.log(Frukter.length)
+
+for (i = 0; i < 5; i++){
+    console.log(Frukter)
+}
+
+
+
+/*
+
+
+map
+
+gör en ny array
+
+push
+
+sätter nya variablar i en gammal array
+
+
+pop
+
+tar bort sista elementet av en array och arrayn blir kortare.
+
+shift
+
+tar bort första elementet och arrayn blir kortare.
+
+unshift
+
+lägger ett nytt första element och arrayn blir längre.
+
+concat
+
+
+lägger ihop 2 olika arrays
+
+
+splice
+
+
+byter saker inne i en array (tar bort eller lägger till)
+
+
+
+
+
+*/
